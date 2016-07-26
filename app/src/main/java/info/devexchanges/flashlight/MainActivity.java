f6db1d14c1cbc68dd4a6ae3725eeac051309173d
DevExchanges <wandered.vip.007@gmail.com>
2016-07-26 15:54:21 +0700

    update project

--- app/src/main/java/info/devexchanges/flashlight/MainActivity.java
+++ app/src/main/java/info/devexchanges/flashlight/MainActivity.java
@@ -1,38 +1,130 @@
 package info.devexchanges.flashlight;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private CameraManager cameraManager;
+    private String cameraId;
     private ImageButton btnSwitch;
+    private boolean isFlashOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        btnSwitch = (ImageButton) findViewById(R.id.btnSwitch);
+        isFlashOn = false;
 
-        btnSwitch = (ImageButton)findViewById(R.id.btnSwitch);
+        boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-        boolean hasFlash =  getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-        if (!hasFlash) {
-            AlertDialog alert = new AlertDialog.Builder(this)
-                    .create();
-            alert.setTitle("Error");
-            alert.setMessage("Sorry, your device doesn't support flash light!");
-            alert.setButton(DialogInterface.BUTTON_NEGATIVE, "OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
+        if (!isFlashAvailable) {
+            AlertDialog alert = new AlertDialog.Builder(this).create();
+            alert.setTitle("Error!");
+            alert.setIcon(android.R.drawable.ic_dialog_alert);
+            alert.setMessage("Your device doesn't support flash light!");
+             alert.setCancelable(false);
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
                     finish();
                 }
             });
             alert.show();
+            return;
+        }
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isFlashOn) {
+                        turnOffFlashLight();
+                        isFlashOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isFlashOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void turnOnFlashLight() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                cameraManager.setTorchMode(cameraId, true);
+                playOnOffSound();
+                btnSwitch.setImageResource(R.drawable.switch_on);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
+    public void turnOffFlashLight() {
 
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                cameraManager.setTorchMode(cameraId, false);
+                playOnOffSound();
+                btnSwitch.setImageResource(R.drawable.switch_off);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playOnOffSound() {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.flash_sound);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+        });
+        mediaPlayer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (isFlashOn) {
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isFlashOn) {
+            turnOnFlashLight();
+        }
+    }
 }
